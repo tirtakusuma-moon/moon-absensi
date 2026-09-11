8a0f9c17dfc150784317cc0c1c4c9a27ebc3cd23
tirtakusuma-moon <tirtakusuma030595@gmail.com>
2026-09-11 17:07:03 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = const SUPABASE_URL = 'https://aucdcaethhzxmuhpcrpd.supabase.co'
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_7Sxa5lX0fNs6qekeTRIfFw_ElWWjPIN'
+const SUPABASE_PUBLISHABLE_KEY = sb_publishable_Fdkca2TLfOEwoeQ4Un95Eg_BWW5jTaY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY)
