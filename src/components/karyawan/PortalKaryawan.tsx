2ebc05d3f36d7db81300c5ed1dbaf146f803d36e
tirtakusuma-moon <tirtakusuma030595@gmail.com>
2026-09-11 16:28:36 +0700

    Update PortalKaryawan.tsx

--- src/components/karyawan/PortalKaryawan.tsx
+++ src/components/karyawan/PortalKaryawan.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 interface Karyawan {
   id: string;
